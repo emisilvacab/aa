7a665cdf20a6033543b2477fcdcf01c2496d71b9
leoomelgaar <Leonardomelgar1@gmail.com>
2024-08-31 18:58:04 -0300

    Add id3 training and evaluation

--- lab1.py
+++ lab1.py
@@ -20,7 +20,7 @@ def id3(dataset, attributes, target_attribute, parent_node_class = None):
 
   if len(numpy.unique(dataset[target_attribute])) == 1:
     # Si todos los ejemplos tienen el mismo valor → etiquetar con ese valor
-    return str(numpy.unique(dataset[target_attribute])[0])
+    return str(dataset[target_attribute])
 
   elif len(attributes) == 0:
     # Si no me quedan atributos → etiquetar con el valor más común
@@ -28,14 +28,14 @@ def id3(dataset, attributes, target_attribute, parent_node_class = None):
 
   else:
     # parent_node_class es usado en caso de que no nos quedan mas atributos en la proxima llamada al id3
-    parent_node_class = determine_majority_class(dataset, target_attribute)
+    parent_node_class = most_common_value(dataset, target_attribute)
 
     dataset_matrix = dataset[attributes].to_numpy()
     classes_vector = dataset[target_attribute].to_numpy()
     index_best_attribute = best_attribute_to_split(dataset_matrix, classes_vector)
     best_attribute = attributes[index_best_attribute]
 
-    tree = {best_attribute: {}}
+    tree = {str(best_attribute): {}}
     remaining_attributes = attributes.copy()
     remaining_attributes.remove(best_attribute)
 
@@ -46,16 +46,16 @@ def id3(dataset, attributes, target_attribute, parent_node_class = None):
 
     return tree
 
-def determine_majority_class(dataset, target_attribute):
+def most_common_value(dataset, target_attribute):
   """
-  Determina la clase mayoritaria en el conjunto de datos.
+  Determina el valor mas comun del target_attribute en el conjunto de datos.
 
   Args:
       dataset (pandas.DataFrame): El conjunto de datos de entrada.
       target_attribute (str): El nombre de la columna que contiene las etiquetas o clases objetivo.
 
   Returns:
-      str: La clase mayoritaria.
+      El valor mas comun del target_attribute en el conjunto de datos.
   """
   return numpy.unique(dataset[target_attribute])[numpy.argmax(numpy.unique(dataset[target_attribute], return_counts = True)[1])]
 
@@ -128,7 +128,7 @@ def train_model(model, dataset_train, target_train):
   """
   model.fit(dataset_train, target_train)
 
-def evaluate_model(model, dataset_test, target_test):
+def evaluate_sklearn_model(model, dataset_test, target_test):
   """
   Evalúa el rendimiento de un modelo de machine learning utilizando un conjunto de datos de prueba.
 
@@ -152,9 +152,57 @@ def evaluate_model(model, dataset_test, target_test):
 
   return accuracy, report
 
-####################################################################################################
+def predict_id3(tree, sample):
+  """
+  Dado una muestra, predice el valor en cierto arbol generado por nuestro algoritmo id3
+  (recordar que el arbol representa una funcion discreta, asi que lo que se esta haciendo
+    es evaluar la funcion predecida en la muestra)
 
-# SCRIPT DE PRUEBA
+  Args:
+      tree: Un arbol generado por id3
+      sample: una muestra (por ejemplo una row del dataset)
+
+  Returns:
+      el valor del target attribute para esa muestra en el arbol tree
+  """
+
+  while isinstance(tree, dict):
+    attribute = list(tree.keys())[0]
+    attribute_value = sample[attribute]
+
+    if attribute_value in tree[attribute]:
+      tree = tree[attribute][attribute_value]
+    else:
+      return None
+
+  return tree
+
+def evaluate_id3_model(tree_id3, dataset_test, target_test, most_common_value):
+  """
+  Evalúa el rendimiento del modelo devuelto por nuestro algoritmo id3
+
+  Args:
+      tree_id3: El arbol devuelto por id3.
+      dataset_test (pd.DataFrame o np.ndarray): El conjunto de características (atributos) para la evaluación.
+      target_test (pd.Series o np.ndarray): El vector de etiquetas (target) correspondiente al conjunto de prueba.
+      most_common_value: valor mas comun en el dataset para el target_attribute
+  """
+
+  predictions = [predict_id3(tree_id3, sample) for _, sample in dataset_test.iterrows()]
+  predictions = [p if p is not None else most_common_value for p in predictions]
+  target_test_for_id3 = target_test.astype(str)
+
+  accuracy = metrics.accuracy_score(target_test_for_id3, predictions)
+  # Ponemos el most common value para las samples no predecidas
+  report = metrics.classification_report(target_test_for_id3, predictions, zero_division = most_common_value)
+
+  print(f"Accuracy: {accuracy}")
+  print("Classification Report:")
+  print(report)
+
+  return accuracy, report
+
+####################################################################################################
 
 # Cargar el archivo CSV
 dataset = pandas.read_csv('lab1_dataset.csv')
@@ -163,34 +211,28 @@ dataset = dataset.drop('pidnum', axis='columns')
 # Definir los atributos y el atributo objetivo
 attributes = dataset.columns.values.tolist()
 attributes.remove('cid')
-
 target_attribute = 'cid'
+mcv = most_common_value(dataset, target_attribute)
 
 # Dividir los datos en conjuntos de entrenamiento y prueba
 dataset_train, dataset_test, target_train, target_test = model_selection.train_test_split(dataset[attributes], dataset[target_attribute], test_size=0.2, random_state=42)
 
-
 ######## Construccion, entrenamiento y evaluacion de modelos #######
 
 # ID3
-id3_model = id3(dataset, attributes, target_attribute)
-
 print("ID3")
-print(id3_model)
-
-# train_model(id3_model, dataset_train, target_train)
-# _accuracy, _report = evaluate_model(id3_model, dataset_test, target_test)
+dataset_train_for_id3 = pandas.concat([dataset_train, target_train], axis=1)
+tree_id3 = id3(dataset_train_for_id3, attributes, target_attribute)
+evaluate_id3_model(tree_id3, dataset_test, target_test, mcv)
 
 # DecisionTreeClassifier
-dtc_model = tree.DecisionTreeClassifier(random_state=42)
-
 print("DecisionTreeClassifier")
+dtc_model = tree.DecisionTreeClassifier(random_state=42)
 train_model(dtc_model, dataset_train, target_train)
-_accuracy, _report = evaluate_model(dtc_model, dataset_test, target_test)
+_accuracy, _report = evaluate_sklearn_model(dtc_model, dataset_test, target_test)
 
 # RandomForestClassifier
-rfc_model = ensemble.RandomForestClassifier(n_estimators=100, random_state=42)  # n_estimators es el número de árboles en el bosque
-
 print("RandomForestClassifier")
+rfc_model = ensemble.RandomForestClassifier(n_estimators=100, random_state=42)  # n_estimators es el número de árboles en el bosque
 train_model(rfc_model, dataset_train, target_train)
-_accuracy, _report = evaluate_model(rfc_model, dataset_test, target_test)
+_accuracy, _report = evaluate_sklearn_model(rfc_model, dataset_test, target_test)
